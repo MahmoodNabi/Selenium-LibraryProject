c266106b2cc799dfd227c0d6c76cb3b37a69f1f7
MahmoodNabi <m.a.nabiyev@gmail.com>
2020-12-14 13:51:21 -0800

    c

--- src/test/java/com/libraryProject/runners/Run.java
+++ src/test/java/com/libraryProject/runners/Run.java
@@ -10,7 +10,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue ="com/libraryProject/step_definition",
         dryRun =false,
-        tags="@logIn",
+        tags="@librarian",
          publish=true
 
 )
